8ed85e916ce389de6c22e447e7caa6bcfdc66e8a
IndraRusyana <indrarusyana@gmail.com>
2025-12-05 14:23:03 +0700

    Set default port to 8080 in index.js and add Dockerfile for containerization

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ const projectRoutes = require('./routes/projectRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
